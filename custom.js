3d308b12537299d5ccac92145e5855c0750ca0ee
allwaysapp <info@allways.app>
2026-04-20 18:35:27 +0300

    Update custom.js

--- custom.js
+++ custom.js
@@ -58,11 +58,11 @@
             <div class="second-content">
                 <div class="second-content-item">
                     <h1>HAFTANIN OYUNU</h1>
-                    <a class="product-btn1" href="${langPrefix}/casino/games/pragmaticplay-fire-stampede-ultimate">HEMEN OYNA</a>
+                    <a class="product-btn1" href="${langPrefix}/casino/games/hacksaw-wanted-dead-or-a-wild">HEMEN OYNA</a>
                 </div>
                 <div class="second-content-item">
-                    <a class="week-game" href="${langPrefix}/casino/games/pragmaticplay-fire-stampede-ultimate"><img
-                        src="https://i.ibb.co/wZKMXQf9/Fire-Stampede-Ultimate-416x560-PP-Logo-EN.png"
+                    <a class="week-game" href="${langPrefix}/casino/games/hacksaw-wanted-dead-or-a-wild"><img
+                        src="https://vendor-provider.fra1.cdn.digitaloceanspaces.com/ebetlab/yHSTi79Pv5V9CNAgBA11WTRRLqJJ1eFD/games/hsCcjGPmGzLJj7hQHQcwym7BgMkfCxh9ENh3Qh2v.png"
                         alt=""></a>
                 </div>
             </div>
@@ -70,10 +70,10 @@
             <div class="second-content">
                 <div class="second-content-item">
                     <h1>HAFTANIN OYUNU</h1>
-                    <a class="week-game" href="${langPrefix}/casino/games/pragmaticplay-fire-stampede-ultimate"><img
-                        src="https://i.ibb.co/wZKMXQf9/Fire-Stampede-Ultimate-416x560-PP-Logo-EN.png"
+                    <a class="week-game" href="${langPrefix}/casino/games/hacksaw-wanted-dead-or-a-wild"><img
+                        src="https://vendor-provider.fra1.cdn.digitaloceanspaces.com/ebetlab/yHSTi79Pv5V9CNAgBA11WTRRLqJJ1eFD/games/hsCcjGPmGzLJj7hQHQcwym7BgMkfCxh9ENh3Qh2v.pngg"
                         alt=""></a>
-                    <a class="product-btn1" href="${langPrefix}/casino/games/pragmaticplay-fire-stampede-ultimate">HEMEN OYNA</a>
+                    <a class="product-btn1" href="${langPrefix}/casino/games/hacksaw-wanted-dead-or-a-wild">HEMEN OYNA</a>
                 </div>
             </div>
         `;
